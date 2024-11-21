80df7322704b2022cafd7b3eedfb086fe534ed55
eranario <ewranario@ucdavis.edu>
2024-11-21 12:04:59 -0800

    updated dataset to align multispec images

--- src/dataset.py
+++ src/dataset.py
@@ -1,57 +1,103 @@
 import os
-import xml.etree.ElementTree as ET
+import cv2
+import numpy as np
 
 from torch.utils.data import Dataset
 from PIL import Image
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
 
 class PotatoDataset(Dataset):
     
-    def __init__(self, img_dir, label_dir, transform=None):
-        """Multispectral Potato Detection and Classification Dataset
-
-        Args:
-            img_dir (str): Directory with all the images.
-            label_dir (str): Directory with all the XML label files.
-            transform (callable, optional): If specified, this transform will be applied on the image.
-        """
-        self.img_dir = img_dir
-        self.label_dir = label_dir
+    def __init__(self, rgb_dir, spectral_dir, transform=None, mode='train'):
+        """Multispectral Potato Detection and Classification Dataset"""
+        self.mode = mode
         self.transform = transform
-        self.img_files = [f for f in os.listdir(img_dir) if f.endswith('.jpg')]
-        
-    def __len__(self):
-        return len(self.img_files)
-    
-    def parse_xml(self, xml_file):
-        """
-        Parse XML file to extract bounding box coordinates and labels.
-        """
-        tree = ET.parse(xml_file)
-        root = tree.getroot()
-        boxes = []
-        labels = []
-        
-        for obj in root.findall('object'):
-            label = obj.find('name').text
-            bndbox = obj.find('bndbox')
-            xmin = int(bndbox.find('xmin').text)
-            ymin = int(bndbox.find('ymin').text)
-            xmax = int(bndbox.find('xmax').text)
-            ymax = int(bndbox.find('ymax').text)
-            boxes.append([xmin, ymin, xmax, ymax])
-            labels.append(label)
-        
-        return boxes, labels
-    
-    def __getitem__(self, idx):
-        img_name = os.path.join(self.img_dir, self.img_files[idx])
-        image = Image.open(img_name).convert("RGB")
-        xml_file = os.path.join(self.label_dir, os.path.splitext(self.img_files[idx])[0] + '.xml')
-        
-        boxes, labels = self.parse_xml(xml_file)
+        self.channels = ['Green_Channel', 'Near_Infrared_Channel', 'Red_Channel', 'Red_Edge_Channel']
 
+        # initialize file lists
+        folder_set = 'Train_Images' if mode == 'train' else 'Test_Images'
+        self.rgb_files = [f for f in os.listdir(os.path.join(rgb_dir, folder_set)) if f.endswith('.jpg')]
+        self.spectral_files = {channel: [
+            f for f in os.listdir(os.path.join(spectral_dir, channel, folder_set)) if f.endswith('.jpg')
+        ] for channel in self.channels}
+
+        # preload and align images using multiprocessing
+        args_list = [
+            (rgb_dir, spectral_dir, folder_set, rgb_name, idx, self.channels, self.spectral_files, self.align_images)
+            for idx, rgb_name in enumerate(self.rgb_files)
+        ]
+
+        with ProcessPoolExecutor() as executor:
+            results = list(tqdm(executor.map(self.process_image, args_list), desc="Loading data", total=len(self.rgb_files)))
+
+        self.data = results
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        rgb_image, aligned_spectral = self.data[idx]
+
+        # convert to PIL
+        rgb_image = Image.fromarray(rgb_image).convert('RGB')
+        aligned_spectral = [Image.fromarray(img) for img in aligned_spectral]
+
+        # apply transformations
         if self.transform:
-            image = self.transform(image)
-        
-        target = {'boxes': boxes, 'labels': labels}
-        return image, target
+            rgb_image = self.transform(rgb_image)
+            aligned_spectral = [self.transform(img) for img in aligned_spectral]
+
+        return (rgb_image, *aligned_spectral)
+    
+    @staticmethod
+    def process_image(args):
+        """
+        Process a single image (alignment and resizing).
+        """
+        rgb_dir, spectral_dir, folder_set, rgb_name, idx, channels, spectral_files, align_images = args
+
+        # read the RGB image
+        rgb_path = os.path.join(rgb_dir, folder_set, rgb_name)
+        rgb_im = cv2.imread(rgb_path)
+
+        # resize RGB to match spectral dimensions
+        height, width = cv2.imread(
+            os.path.join(spectral_dir, channels[0], folder_set, spectral_files[channels[0]][idx]),
+            cv2.IMREAD_GRAYSCALE
+        ).shape
+        rgb_resized = cv2.resize(rgb_im, (width, height), interpolation=cv2.INTER_LINEAR)
+        rgb_gray = cv2.cvtColor(rgb_resized, cv2.COLOR_BGR2GRAY)
+
+        # align spectral images
+        aligned_spectral = []
+        for channel in channels:
+            spectral_path = os.path.join(spectral_dir, channel, folder_set, spectral_files[channel][idx])
+            spectral_im = cv2.imread(spectral_path, cv2.IMREAD_GRAYSCALE)
+            aligned_image = align_images(rgb_gray, spectral_im)
+            aligned_spectral.append(aligned_image)
+
+        return (rgb_resized, aligned_spectral)
+
+    @staticmethod
+    def align_images(base_img, img_to_align):
+        """
+        Align img_to_align to base_img using ORB keypoints.
+        """
+        orb = cv2.ORB_create(5000)
+        keypoints1, descriptors1 = orb.detectAndCompute(base_img, None)
+        keypoints2, descriptors2 = orb.detectAndCompute(img_to_align, None)
+
+        # match features
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        matches = bf.match(descriptors1, descriptors2)
+        matches = sorted(matches, key=lambda x: x.distance)
+
+        # extract points
+        points1 = np.float32([keypoints1[m.queryIdx].pt for m in matches])
+        points2 = np.float32([keypoints2[m.trainIdx].pt for m in matches])
+
+        # homography
+        h, _ = cv2.findHomography(points2, points1, cv2.RANSAC)
+        aligned_img = cv2.warpPerspective(img_to_align, h, (base_img.shape[1], base_img.shape[0]))
+        return aligned_img
