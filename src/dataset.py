a5e26493f333cb0b543aceb83d047609fa70ac46
eranario <ewranario@ucdavis.edu>
2024-11-26 14:44:51 -0800

    add condition to align images

--- src/dataset.py
+++ src/dataset.py
@@ -1,18 +1,18 @@
 import os
 import cv2
 import numpy as np
-
 from torch.utils.data import Dataset
 from PIL import Image
 from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor
 
 class PotatoDataset(Dataset):
-    
-    def __init__(self, rgb_dir, spectral_dir, transform=None, mode='train'):
+
+    def __init__(self, rgb_dir, spectral_dir, transform=None, mode='train', align=True):
         """Multispectral Potato Detection and Classification Dataset"""
         self.mode = mode
         self.transform = transform
+        self.align = align
         self.channels = ['Green_Channel', 'Near_Infrared_Channel', 'Red_Channel', 'Red_Edge_Channel']
 
         # initialize file lists
@@ -24,7 +24,7 @@ class PotatoDataset(Dataset):
 
         # preload and align images using multiprocessing
         args_list = [
-            (rgb_dir, spectral_dir, folder_set, rgb_name, idx, self.channels, self.spectral_files, self.align_images)
+            (rgb_dir, spectral_dir, folder_set, rgb_name, idx, self.channels, self.spectral_files, self.align)
             for idx, rgb_name in enumerate(self.rgb_files)
         ]
 
@@ -37,25 +37,25 @@ class PotatoDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        rgb_image, aligned_spectral = self.data[idx]
+        rgb_image, spectral_images = self.data[idx]
 
         # convert to PIL
         rgb_image = Image.fromarray(rgb_image).convert('RGB')
-        aligned_spectral = [Image.fromarray(img) for img in aligned_spectral]
+        spectral_images = [Image.fromarray(img) for img in spectral_images]
 
         # apply transformations
         if self.transform:
             rgb_image = self.transform(rgb_image)
-            aligned_spectral = [self.transform(img) for img in aligned_spectral]
+            spectral_images = [self.transform(img) for img in spectral_images]
 
-        return (rgb_image, *aligned_spectral)
-    
+        return (rgb_image, *spectral_images)
+
     @staticmethod
     def process_image(args):
         """
         Process a single image (alignment and resizing).
         """
-        rgb_dir, spectral_dir, folder_set, rgb_name, idx, channels, spectral_files, align_images = args
+        rgb_dir, spectral_dir, folder_set, rgb_name, idx, channels, spectral_files, align = args
 
         # read the RGB image
         rgb_path = os.path.join(rgb_dir, folder_set, rgb_name)
@@ -69,22 +69,25 @@ class PotatoDataset(Dataset):
         rgb_resized = cv2.resize(rgb_im, (width, height), interpolation=cv2.INTER_LINEAR)
         rgb_gray = cv2.cvtColor(rgb_resized, cv2.COLOR_BGR2GRAY)
 
-        # align spectral images
-        aligned_spectral = []
+        # process spectral images
+        spectral_images = []
         for channel in channels:
             spectral_path = os.path.join(spectral_dir, channel, folder_set, spectral_files[channel][idx])
             spectral_im = cv2.imread(spectral_path, cv2.IMREAD_GRAYSCALE)
-            aligned_image = align_images(rgb_gray, spectral_im)
-            aligned_spectral.append(aligned_image)
+            if align:
+                aligned_image = PotatoDataset.align_images(rgb_gray, spectral_im)
+                spectral_images.append(aligned_image)
+            else:
+                spectral_images.append(spectral_im)
 
-        return (rgb_resized, aligned_spectral)
+        return (rgb_resized, spectral_images)
 
     @staticmethod
     def align_images(base_img, img_to_align):
         """
         Align img_to_align to base_img using ORB keypoints.
         """
-        orb = cv2.ORB_create(5000)
+        orb = cv2.ORB_create(6000)
         keypoints1, descriptors1 = orb.detectAndCompute(base_img, None)
         keypoints2, descriptors2 = orb.detectAndCompute(img_to_align, None)
 
