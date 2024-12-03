b4e60bd60b578bad18092e11fe81eb2af5c17671
eranario <ewranario@ucdavis.edu>
2024-12-03 14:07:16 -0800

    added vae attempt

--- src/model.py
+++ src/model.py
@@ -33,18 +33,25 @@ class Downsample(nn.Module):
         return down, p
     
 class Upsample(nn.Module):
-    
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        
-        self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
         self.double_conv = DoubleConv(in_channels, out_channels)
-        
-    def forward(self, x1, x2):
+        self.double_conv_half = DoubleConv(in_channels // 2, out_channels)
+
+    def forward(self, x1, x2=None):
         x1 = self.up(x1)
-        x = torch.cat([x1, x2], 1)
-        
-        return self.double_conv(x)
+        if x2 is not None:
+            x = torch.cat([x1, x2], dim=1)
+            x = self.double_conv(x)
+        else:
+            x = x1  # No skip connection
+            x = self.double_conv_half(x)
+        return x
+    
+##############################################
+############### UNET SETUP ###################
+##############################################
     
 class UNet(nn.Module):
     
@@ -89,3 +96,69 @@ class UNet(nn.Module):
         # output layer
         out = self.out(up_4)
         return out
+    
+##############################################
+################ VAE SETUP ###################
+##############################################
+
+class VAE(nn.Module):
+    
+    def __init__(self, in_channels, out_channels, latent_dim):
+        super().__init__()
+        
+        # encoder
+        self.down_conv_1 = Downsample(in_channels, out_channels=64)
+        self.down_conv_2 = Downsample(in_channels=64, out_channels=128)
+        self.down_conv_3 = Downsample(in_channels=128, out_channels=256)
+        self.down_conv_4 = Downsample(in_channels=256, out_channels=512)
+        
+        # bottleneck
+        self.bottleneck_mu = nn.Linear(512*14*14, latent_dim) # mean
+        self.bottleneck_logvar = nn.Linear(512*14*14, latent_dim) # log variance
+        
+        # decoder
+        self.decoder_input = nn.Linear(latent_dim, 512*14*14)
+        self.up_conv_1 = Upsample(in_channels=512, out_channels=256)
+        self.up_conv_2 = Upsample(in_channels=256, out_channels=128)
+        self.up_conv_3 = Upsample(in_channels=128, out_channels=64)
+        self.up_conv_4 = Upsample(in_channels=64, out_channels=32)
+        
+        # output layer
+        self.out = nn.Conv2d(in_channels=32, out_channels=out_channels, kernel_size=1)
+        
+    def reparameterize(self, mu, logvar):
+        """To sample from N(mu, var) using N(0, 1)"""
+        std = torch.exp(logvar/2)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+    
+    def encode(self, x):
+        """Encode input into latent space"""
+        _, p1 = self.down_conv_1(x)
+        _, p2 = self.down_conv_2(p1)
+        _, p3 = self.down_conv_3(p2)
+        _, p4 = self.down_conv_4(p3)
+
+        # flatten
+        p4 = p4.view(p4.size(0), -1)
+        mu = self.bottleneck_mu(p4)
+        logvar = self.bottleneck_logvar(p4)
+        return mu, logvar
+    
+    def decode(self, z):
+        """Decode latent space into output"""
+        x = self.decoder_input(z)
+        x = x.view(x.size(0), 512, 14, 14)
+        
+        up_1 = self.up_conv_1(x, None)
+        up_2 = self.up_conv_2(up_1, None)
+        up_3 = self.up_conv_3(up_2, None)
+        up_4 = self.up_conv_4(up_3, None)
+        
+        out = self.out(up_4)
+        return out
+    
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
