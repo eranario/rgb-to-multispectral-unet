f330c74143b58dee5d76d76639cd69eb633ab601
eranario <ewranario@ucdavis.edu>
2024-12-08 16:19:39 -0800

    attempt to integrate spectral signal

--- src/model.py
+++ src/model.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+
 class DoubleConv(nn.Module):
     
     def __init__(self, in_channels, out_channels):
@@ -52,134 +54,165 @@ class Upsample(nn.Module):
         return x
     
 class TransformerBlock(nn.Module):
-    
-    def __init__(self, dim, num_heads, mlp_dim, dropout=0.1):
-        super().__init__()
-        self.norm1 = nn.LayerNorm(dim)
-        self.attn = nn.MultiheadAttention(dim, num_heads, dropout=dropout)
-        self.norm2 = nn.LayerNorm(dim)
-        self.mlp = nn.Sequential(
-            nn.Linear(dim, mlp_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(mlp_dim, dim),
-            nn.Dropout(dropout)
+    def __init__(self, embed_dim, num_heads, ff_dim, dropout=0.3):
+        super(TransformerBlock, self).__init__()
+        self.transformer_layer = TransformerEncoderLayer(
+            d_model=embed_dim,
+            nhead=num_heads,
+            dim_feedforward=ff_dim,
+            dropout=dropout,
+            activation="relu"
         )
-        
+        self.transformer = TransformerEncoder(self.transformer_layer, num_layers=1)
+
     def forward(self, x):
-        # self attention
-        x = self.norm1(x)
-        x = x + self.attn(x, x, x)[0]
+        # Flatten spatial dimensions to sequence (B, C, H, W) -> (B, C, HW) -> (HW, B, C)
+        b, c, h, w = x.size()
+        x = x.view(b, c, h * w).permute(2, 0, 1)
         
-        # feed forward
-        x = self.norm2(x)
-        x = x + self.mlp(x)
-        return x
+        # Apply transformer
+        x = self.transformer(x)
         
-class Transformer(nn.Module):
-    
-    def __init__(self, dim, depth, num_heads, mlp_dim, dropout=0.1):
-        super().__init__()
-        self.layers = nn.ModuleList([
-            TransformerBlock(dim, num_heads, mlp_dim, dropout) for _ in range(depth)
-        ])
-        
-    def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
+        # Reshape back to spatial dimensions (HW, B, C) -> (B, C, H, W)
+        x = x.permute(1, 2, 0).view(b, c, h, w)
         return x
     
-class DownsampleTR(nn.Module):
-    def __init__(self, in_channels, out_channels, depth, num_heads, mlp_dim=2048, dropout=0.1):
-        super().__init__()
+class TransformerBlockSpectral(nn.Module):
+    def __init__(self, embed_dim, num_heads, ff_dim, dropout=0.3):
+        super(TransformerBlockSpectral, self).__init__()
         
-        self.max_pool = nn.MaxPool1d(kernel_size=2, stride=2)
-        self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.transformer = Transformer(dim=out_channels, depth=depth, num_heads=num_heads, mlp_dim=mlp_dim, dropout=dropout)
-        self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.relu2 = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        
-        x = self.max_pool(x)
-        x = self.relu1(self.conv1(x))
-        x = x.permute(0, 2, 1)
-        p = self.transformer(x)
-        p = p.permute(0, 2, 1)
-        x = self.relu2(self.conv2(p))
-        return x
-
-class BottleneckTR(nn.Module):
-    def __init__(self, in_channels, out_channels, depth, num_heads, mlp_dim=2048, dropout=0.1):
-        super().__init__()
-        
-        # self.adaptive_pool = nn.AdaptiveMaxPool1d(output_size=25) # TODO: hard-coded at the moment (Maybe remove?)
-        self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.transformer = Transformer(dim=out_channels, depth=depth, num_heads=num_heads, mlp_dim=mlp_dim, dropout=dropout)
-        self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.relu2 = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        
-        # x = self.adaptive_pool(x)
-        # print(f"Max pool shape: {x.shape}")
-        x = self.relu1(self.conv1(x))
-        x = x.permute(0, 2, 1)
-        p = self.transformer(x)
-        p = p.permute(0, 2, 1)
-        x = self.relu2(self.conv2(p))
-        
-        return x
-    
-class ProjectionTR(nn.Module):
-    def __init__(self, in_channels):
-        super().__init__()
-
-        self.proj = nn.Sequential(
-            nn.Conv1d(in_channels, in_channels * 2, kernel_size=3, padding=1),
-            nn.BatchNorm1d(in_channels * 2),
+        # transformer layers
+        self.self_attn = nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout)
+        self.cross_attn = nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_dim, ff_dim),
             nn.ReLU(),
-            nn.Conv1d(in_channels * 2, in_channels*4, kernel_size=1),
-            nn.BatchNorm1d(in_channels*4),
-            nn.ReLU()
+            nn.Linear(ff_dim, embed_dim)
         )
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.norm3 = nn.LayerNorm(embed_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x, signal=None):
+        
+        # self attention
+        b, c, h, w = x.size()
+        x = x.view(b, c, h * w).permute(2, 0, 1)
+        attn_output, _ = self.self_attn(x, x, x)
+        x = x + self.dropout(attn_output)
+        x = self.norm1(x)
+        
+        # cross attention
+        if signal is not None:
+            signal = signal.permute(1, 0, 2)
+            attn_output, _ = self.cross_attn(x, signal, signal) # TODO: try V as image
+            x = x + self.dropout(attn_output)
+            x = self.norm2(x)
+            
+        # feed forward
+        x = x.permute(1, 0, 2)
+        ff_output = self.feed_forward(x)
+        x = x + self.dropout(ff_output)
+        x = self.norm3(x)
+        
+        return x
+    
+class SpectralEmbedding(nn.Module):
+    
+    def __init__(self, input_dim, embed_dim, num_tokens):
+        super(SpectralEmbedding, self).__init__()
 
+        self.embed_dim = embed_dim
+        
+        # ensure input_dim is divisible by num_tokens
+        if input_dim % num_tokens != 0:
+            # find the closest number of tokens that ensures divisibility
+            num_tokens = self.find_closest_divisible(input_dim, num_tokens)
+            print(f"Adjusted num_tokens to {num_tokens} to make input_dim divisible.")
+        
+        self.num_tokens = num_tokens
+        self.token_size = input_dim // self.num_tokens
+        
+        self.proj = nn.Linear(self.token_size, embed_dim) # TODO: Maybe add non-linearity
+        
+    @staticmethod
+    def find_closest_divisible(input_dim, num_tokens):
+        """
+        Find the closest number to num_tokens that ensures divisibility of input_dim.
+        """
+        upper = num_tokens
+        while upper < input_dim:
+            if input_dim % upper == 0:
+                return upper
+            upper += 1
+        return num_tokens  # fallback (shouldn't be reached)
+        
     def forward(self, x):
-        x = self.proj(x)
         
-        return x
-    
-class UpsampleTR(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        
-        self.up = nn.Sequential(
-            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-        )
+        # split into tokens
+        bs, _, _ = x.shape
+        x = x.view(bs, self.num_tokens, self.token_size) # bs, num_tokens, token_size
 
-    def forward(self, x):
-        x = self.up(x)
+        # apply linear transformation
+        x = self.proj(x) # bs, num_tokens, embed_dim
+        x = x.permute(0, 2, 1)
+    
         return x
     
-class DoubleConv1D(nn.Module):
+##############################################
+############## UNET with TR ##################
+##############################################
+
+class UNeTransformed(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super().__init__()
+        super(UNeTransformed, self).__init__()
         
-        # two convolutions with 1x1 kernels
-        self.conv_op = nn.Sequential(
-            nn.Conv1d(in_channels, out_channels, kernel_size=1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(out_channels, out_channels, kernel_size=1),
-            nn.ReLU(inplace=True)
-        )
+        # Decoder
+        self.down_conv_1 = Downsample(in_channels, out_channels=64)
+        self.trans_1 = TransformerBlock(embed_dim=64, num_heads=1, ff_dim=256)
+        self.down_conv_2 = Downsample(in_channels=64, out_channels=128)
+        self.trans_2 = TransformerBlock(embed_dim=128, num_heads=1, ff_dim=512)
+        self.down_conv_3 = Downsample(in_channels=128, out_channels=256)
+        self.trans_3 = TransformerBlock(embed_dim=256, num_heads=2, ff_dim=1024)
+        self.down_conv_4 = Downsample(in_channels=256, out_channels=512)
+        self.trans_4 = TransformerBlock(embed_dim=512, num_heads=2, ff_dim=2048)
+        
+        # Bottleneck
+        self.bottle_neck = DoubleConv(in_channels=512, out_channels=1024)
+        
+        # Encoder
+        self.up_conv_1 = Upsample(in_channels=1024, out_channels=512)
+        self.up_conv_2 = Upsample(in_channels=512, out_channels=256)
+        self.up_conv_3 = Upsample(in_channels=256, out_channels=128)
+        self.up_conv_4 = Upsample(in_channels=128, out_channels=64)
+        
+        # Output layer
+        self.out = nn.Conv2d(in_channels=64, out_channels=out_channels, kernel_size=1)
     
     def forward(self, x):
-        x = self.conv_op(x)
-        return x
+        # Decoder
+        down_1, p1 = self.down_conv_1(x)
+        trans_1 = self.trans_1(down_1)
+        down_2, p2 = self.down_conv_2(p1)
+        trans_2 = self.trans_2(down_2)
+        down_3, p3 = self.down_conv_3(p2)
+        trans_3 = self.trans_3(down_3)
+        down_4, p4 = self.down_conv_4(p3)
+        trans_4 = self.trans_4(down_4)
+        
+        # Bottleneck
+        b = self.bottle_neck(p4)
+        
+        # Encoder
+        up_1 = self.up_conv_1(b, trans_4)
+        up_2 = self.up_conv_2(up_1, trans_3)
+        up_3 = self.up_conv_3(up_2, trans_2)
+        up_4 = self.up_conv_4(up_3, trans_1)
+        
+        # Output layer
+        out = self.out(up_4)
+        return out
     
 ##############################################
 ############### UNET SETUP ###################
@@ -373,120 +406,63 @@ class Discriminator(nn.Module):
         return out
     
 ##############################################
-############## UNETR SETUP ###################
+############ UNET with TR/Spectra ############
 ##############################################
 
-class UNETR(nn.Module):
-    
-    def __init__(self, in_channels, out_channels, img_size=224, feature_size=32, hidden_size=32, mlp_dim=2048, num_heads=8, num_layers=8, dropout=0.1):
-        """UNet model with Transformers blocks sprinkled in!
-
-        Args:
-            in_channels (int): Number of input channels (e.g., 3 for RGB images or 1 for grayscale images).
-            out_channels (int): Number of output channels.
-            img_size (int, optional): Input image size (assumes square images). Defaults to 224.
-            feature_size (int, optional): Number of feature maps in the first layer of the UNet encoder. Defaults to 32.
-            hidden_size (int, optional): Dimensionality of the Transformer embedding space. Defaults to 768.
-            mlp_dim (int, optional): Dimensionality of the MLP (feedforward) layer in the Transformer. Defaults to 2048.
-            num_heads (int, optional): Number of attention heads in the Transformer. Defaults to 12.
-            num_layers (int, optional): Number of Transformer layers. Defaults to 12.
-            dropout (float, optional): Dropout rate for Transformer layers. Defaults to 0.1.
-        """
-        super().__init__()
+class UNeTransformedSpectral(nn.Module):
+    def __init__(self, in_channels, out_channels, num_bands, spectral_dim=768, num_tokens=196):
+        super(UNeTransformedSpectral, self).__init__()
         
-        self.patch_size = 16 # TODO: hard-coded to match pooling requirements
-        self.hidden_size = hidden_size
-        self.img_size = img_size
-        self.num_patches = (img_size // self.patch_size) ** 2
-        self.feature_size = feature_size
-        self.transformer_args = {
-            "depth": num_layers,
-            "num_heads": num_heads,
-            "mlp_dim": mlp_dim,
-            "dropout": dropout
-        }
+        # spectral embedding
+        self.spectral_embed = SpectralEmbedding(input_dim=num_bands, embed_dim=spectral_dim, num_tokens=num_tokens)
         
-        # positional Embeddings
-        self.positional_embeddings = nn.Parameter(torch.zeros(1, hidden_size, self.num_patches))
-
-        # patch embedding
-        self.patch_embedding = nn.Conv2d(in_channels, hidden_size, kernel_size=self.patch_size, stride=self.patch_size)
-        
-        # encoder
-        self.encoder1 = DoubleConv1D(hidden_size, feature_size)
-        self.encoder2 = DownsampleTR(
-            in_channels=feature_size, out_channels=feature_size*2, **self.transformer_args
-        )
-        self.encoder3 = DownsampleTR(
-            in_channels=feature_size*2, out_channels=feature_size*4, **self.transformer_args
-        )
+        # decoder
+        self.down_conv_1 = Downsample(in_channels, out_channels=64)
+        self.trans_1 = TransformerBlockSpectral(embed_dim=64, num_heads=1, ff_dim=256)
+        self.down_conv_2 = Downsample(in_channels=64, out_channels=128)
+        self.trans_2 = TransformerBlockSpectral(embed_dim=128, num_heads=1, ff_dim=512)
+        self.down_conv_3 = Downsample(in_channels=128, out_channels=256)
+        self.trans_3 = TransformerBlockSpectral(embed_dim=256, num_heads=2, ff_dim=1024)
+        self.down_conv_4 = Downsample(in_channels=256, out_channels=512)
+        self.trans_4 = TransformerBlockSpectral(embed_dim=512, num_heads=2, ff_dim=2048)
         
         # bottleneck
-        self.bottleneck = BottleneckTR(
-            in_channels=feature_size*4, out_channels=feature_size*8, **self.transformer_args
-        )
+        self.bottle_neck = DoubleConv(in_channels=512, out_channels=1024)
         
-        # projection for upsampling
-        self.projection = ProjectionTR(in_channels=feature_size*8)
-        
-        # upsample
-        self.decoder1 = UpsampleTR(in_channels=feature_size*32, out_channels=feature_size*16)
-        self.decoder2 = UpsampleTR(in_channels=feature_size*16, out_channels=feature_size*8)
-        self.decoder3 = UpsampleTR(in_channels=feature_size*8, out_channels=feature_size*4)
-        self.decoder4 = UpsampleTR(in_channels=feature_size*4, out_channels=feature_size*2)
+        # encoder
+        self.up_conv_1 = Upsample(in_channels=1024, out_channels=512)
+        self.up_conv_2 = Upsample(in_channels=512, out_channels=256)
+        self.up_conv_3 = Upsample(in_channels=256, out_channels=128)
+        self.up_conv_4 = Upsample(in_channels=128, out_channels=64)
         
         # output layer
-        self.out = nn.ConvTranspose2d(
-            in_channels=feature_size*2, 
-            out_channels=out_channels, 
-            kernel_size=4, 
-            stride=2,
-            padding=1
-        )
-        
-    def forward(self, x):
-        
-        ### patch embedding ###
-        x = self.patch_embedding(x)
-        x = x.flatten(2)
-        # print(f"Patch embedding shape: {x.shape}")
-        
-        ### add positional embeddings ###
-        x = x + self.positional_embeddings
-        # print(f"Positional embeddings shape: {x.shape}")
-        
-        ### encoder ###
-        enc1 = self.encoder1(x)
-        # print(f"Encoder 1 shape: {enc1.shape}")
-        enc2 = self.encoder2(enc1)
-        # print(f"Encoder 2 shape: {enc2.shape}")
-        enc3 = self.encoder3(enc2)
-        # print(f"Encoder 3 shape: {enc3.shape}")
-        
-        ### bottleneck ###
-        bottle = self.bottleneck(enc3)
-        # print(f"Bottleneck shape: {bottle.shape}")
-        
-        ### projection ###
-        proj = self.projection(bottle)
-        # print(f"Projection shape: {proj.shape}")
-        seq_len = proj.size(2)
-        spatial_dim = int(seq_len ** 0.5)
-        proj = proj.view(proj.size(0), proj.size(1), spatial_dim, spatial_dim)
-        # print(f"Projection reshaped shape: {proj.shape}")
-        
-        ### upsample ###
-        dec1 = self.decoder1(proj)
-        # print(f"Decoder 1 shape: {dec1.shape}")
-        dec2 = self.decoder2(dec1)
-        # print(f"Decoder 2 shape: {dec2.shape}")
-        dec3 = self.decoder3(dec2)
-        # print(f"Decoder 3 shape: {dec3.shape}")
-        dec4 = self.decoder4(dec3)
-        # print(f"Decoder 4 shape: {dec4.shape}")
-        
-        ### output layer ###
-        out = self.out(dec4)
-        # print(f"Output shape: {out.shape}")
+        self.out = nn.Conv2d(in_channels=64, out_channels=out_channels, kernel_size=1)
     
+    def forward(self, x, spectral_signal):
+        
+        # create spectral embedding
+        spectral_signal = spectral_signal.to(dtype=torch.float)
+        spectral_signal = self.spectral_embed(spectral_signal)
+        
+        # decoder
+        down_1, p1 = self.down_conv_1(x)
+        trans_1 = self.trans_1(down_1, spectral_signal)
+        down_2, p2 = self.down_conv_2(p1)
+        trans_2 = self.trans_2(down_2, spectral_signal)
+        down_3, p3 = self.down_conv_3(p2)
+        trans_3 = self.trans_3(down_3, spectral_signal)
+        down_4, p4 = self.down_conv_4(p3)
+        trans_4 = self.trans_4(down_4, spectral_signal)
+        
+        # bottleneck
+        b = self.bottle_neck(p4)
+        
+        # encoder
+        up_1 = self.up_conv_1(b, trans_4)
+        up_2 = self.up_conv_2(up_1, trans_3)
+        up_3 = self.up_conv_3(up_2, trans_2)
+        up_4 = self.up_conv_4(up_3, trans_1)
+        
+        # output layer
+        out = self.out(up_4)
         return out
